90a2b6082acfb364082d0749bc7f2bd13f0293cb
StastkaJan <57797800+StastkaJan@users.noreply.github.com>
2021-03-19 00:01:58 +0100

    ajax test v0.3
    included header and footer

---
+++ ajax.js
@@ -0,0 +1,19 @@
+window.addEventListener('DOMContentLoaded', () => {
+    loadDoc();
+});
+
+function loadDoc() {
+    let items = ['header', 'footer'];
+    for (let i = 0; i < items.length; i++) {
+        let item = items[i];
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                document.getElementById(item).innerHTML =
+                    this.responseText;
+            }
+        };
+        xhttp.open('GET', item + '.html', true);
+        xhttp.send();
+    }
+}
