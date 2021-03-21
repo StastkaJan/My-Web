8217c3777b7afa95054ea4b04fa12ca44f5bc6d9
StastkaJan <57797800+StastkaJan@users.noreply.github.com>
2021-03-21 16:01:57 +0100

    Add files via upload

--- script/ajax.js
+++ script/ajax.js
@@ -1,13 +1,10 @@
-function loadDoc(x) {
-    for (let i = 0; i < x.length; i++) {
-        var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function () {
-            if (this.readyState == 4 && this.status == 200) {
-                document.getElementById(x[i]).innerHTML =
-                    this.responseText;
-            }
-        };
-        xhttp.open('GET', '/templates/' + x[i] + '.html', true);
-        xhttp.send();
-    }
+function ajax(x) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            this = this.responseText;
+        }
+    };
+    xhttp.open('GET', '/templates/' + x + '.html', true);
+    xhttp.send();
 }
