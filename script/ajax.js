107a53dda16a409248ff91c8d5ccbc90950a51de
StastkaJan <57797800+StastkaJan@users.noreply.github.com>
2021-03-21 14:31:42 +0100

    ajax update

--- script/ajax.js
+++ script/ajax.js
@@ -1,14 +1,13 @@
 function loadDoc(x) {
     for (let i = 0; i < x.length; i++) {
-        let item = x[i];
         var xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function () {
             if (this.readyState == 4 && this.status == 200) {
-                document.getElementById(item).innerHTML =
+                document.getElementById(x[i]).innerHTML =
                     this.responseText;
             }
         };
-        xhttp.open('GET', '/templates/' + item + '.html', true);
+        xhttp.open('GET', '/templates/' + x[i] + '.html', true);
         xhttp.send();
     }
 }
