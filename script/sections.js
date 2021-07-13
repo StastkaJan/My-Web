9c4da368c50df8bf6ba79e973286f13b30a714b7
StastkaJan <57797800+StastkaJan@users.noreply.github.com>
2021-07-13 13:59:30 +0200

    Add files via upload

--- script/sections.js
+++ script/sections.js
@@ -1,43 +1,64 @@
 window.addEventListener('DOMContentLoaded', () => {
-    checkSectioons();
+
+    checkSections();
+
 });
 
-function checkSectioons() {
+function checkSections() {
+
     checkHeader();
+
 }
 
 function checkHeader() {
+
     let header;
 
     if (document.querySelector('header').classList[0] != undefined) {
+
         switch (document.querySelector('header').classList[0]) {
+
             case 'mobileHeader':
                 header = 'mobileHeader';
                 break;
+
             case 'header':
                 header = 'header';
                 break;
 
             default:
                 break;
+
         }
+
     }
 
     if (header != undefined) {
-        document.getElementsByClassName(header)[0].innerHTML =
-            httpRequest('GET', '/script/' + header + '.json').code;
+
+        httpRequest('GET', header);
+
     }
 }
 
 function httpRequest(method, address) {
+
     let request = new XMLHttpRequest();
-    request.open(method, address);
+
+    request.open(method, '/script/' + address + '.json');
 
     request.onload = function () {
-        console.log(request.response);
-        console.log(JSON.parse(request.response));
-        return JSON.parse(request.response);
+
+        let json = JSON.parse(request.response);
+        printToDoc(json, address);
+        
     }
 
     request.send();
+
+}
+
+function printToDoc(value, address) {
+
+    document.getElementsByClassName(address)[0].innerHTML = value.code;
+    
 }
